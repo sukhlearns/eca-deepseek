bc211c2a1a4ec5093c180c898e3d2cd1ae1e22a1
Richard <richardramos112094@gmail.com>
2025-02-08 03:02:39 +0800

    updated

--- src/pages/api/ask.ts
+++ src/pages/api/ask.ts
@@ -92,7 +92,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(200).json({ answer, imageUrl, sessionId: session });
         
 
-        console.log(ollamaResponse.data);
+        // console.log(ollamaResponse.data);
 
 
 
